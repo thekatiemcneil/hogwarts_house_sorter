475646c23a08a2cdaefffc5ee6c389229118390c
thekatiemcneil <katie.mcneil@live.com>
2017-06-07 18:49:23 -0500

    Add sql as a required gem on the ruby doc. Add methods to create a database and create tables for students and houses.

--- quiz.rb
+++ quiz.rb
@@ -1,16 +1,45 @@
 # THE SORTING HAT
 
-# require gems
+# require gems (squlite3)
+# require 'sqlite3'
 
 # create SQLite3 database
+# db = SQLite3::Database.new("sorting_hat.db")
 
 # make table using create table cmd
+# create_student_table = <<-SQL
+  # CREATE TABLE IF NOT EXISTS students(
+    # id INTEGER PRIMARY KEY,
+    # name VARCHAR(225),
+    # gender VARCHAR(225),
+    # house_id INT,
+    # year INT,
+    # quidditch_interest BOOLEAN,
+    # 2nd_house_id INT,
+    # 3rd_house_id INT,
+    # 4th_house_id INT,
+    # FOREIGN KEY (house_id) REFERENCES houses(id),
+    # FOREIGN KEY (2nd_house_id) REFERENCES houses(id),
+    # FOREIGN KEY (3rd_house_id) REFERENCES houses(id),
+    # FOREIGN KEY (4th_house_id) REFERENCES houses(id)
+  # )
+# SQL
 
 # Create students table (do I need to use the create_table_cmd that they used?)
+# done above
 
 # Create houses table
 
+# create_houses_table <<-SQL
+  #   CREATE TABLE IF NOT EXISTS houses(
+    #   id INTEGER PRIMARY KEY,
+    #   name VARCHAR(225),
+    #   number_of_students INT
+  #   )
+# SQL
+
 # Write methods for the actual quiz...ie how the question answers sort the user
+#
 
 # Write method to create a user
 
