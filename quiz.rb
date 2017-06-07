eb9feff2c98341f8b9a32a47afa79c603dbe0128
thekatiemcneil <katie.mcneil@live.com>
2017-06-07 18:27:57 -0500

    Begin adding general steps and layout of program.

---
+++ quiz.rb
@@ -0,0 +1,12 @@
+# THE SORTING HAT
+
+# require gems
+
+# create SQLite3 database
+
+# make table using create table cmd
+
+# Create students table (do I need to use the create_table_cmd that they used?)
+
+# Create houses table
+
