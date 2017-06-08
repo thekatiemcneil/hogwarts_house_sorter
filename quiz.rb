952bf1460d18825e3d282c4c2bc9afb335e2de9a
thekatiemcneil <katie.mcneil@live.com>
2017-06-07 19:21:03 -0500

    Remove commenting for intial methods to create and populate databases.

--- quiz.rb
+++ quiz.rb
@@ -1,58 +1,63 @@
 # THE SORTING HAT
 
 # require gems (squlite3)
-# require 'sqlite3'
+require 'sqlite3'
 
 # create SQLite3 database
-# db = SQLite3::Database.new("sorting_hat.db")
+db = SQLite3::Database.new("sorting_hat.db")
 
 # make table using create table cmd
-# create_student_table = <<-SQL
-  # CREATE TABLE IF NOT EXISTS students(
-    # id INTEGER PRIMARY KEY,
-    # name VARCHAR(225),
-    # gender VARCHAR(225),
-    # house_id INT,
-    # year INT,
-    # quidditch BOOLEAN,
-    # 2nd_house_id INT,
-    # 3rd_house_id INT,
-    # 4th_house_id INT,
-    # FOREIGN KEY (house_id) REFERENCES houses(id),
-    # FOREIGN KEY (2nd_house_id) REFERENCES houses(id),
-    # FOREIGN KEY (3rd_house_id) REFERENCES houses(id),
-    # FOREIGN KEY (4th_house_id) REFERENCES houses(id)
-  # )
-# SQL
+create_student_table = <<-SQL
+  CREATE TABLE IF NOT EXISTS students(
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(225),
+    gender VARCHAR(225),
+    house_id INT,
+    year INT,
+    quidditch BOOLEAN,
+    2nd_house_id INT,
+    3rd_house_id INT,
+    4th_house_id INT,
+    FOREIGN KEY (house_id) REFERENCES houses(id),
+    FOREIGN KEY (2nd_house_id) REFERENCES houses(id),
+    FOREIGN KEY (3rd_house_id) REFERENCES houses(id),
+    FOREIGN KEY (4th_house_id) REFERENCES houses(id)
+  )
+SQL
 
 # Create students table (do I need to use the create_table_cmd that they used?)
 # done above
 
 # Create houses table
 
-# create_houses_table <<-SQL
-  #   CREATE TABLE IF NOT EXISTS houses(
-    #   id INTEGER PRIMARY KEY,
-    #   name VARCHAR(225),
-    #   number_of_students INT
-  #   )
-# SQL
+create_houses_table <<-SQL
+    CREATE TABLE IF NOT EXISTS houses(
+      id INTEGER PRIMARY KEY,
+      name VARCHAR(225),
+      number_of_students INT
+    )
+SQL
+
+db.execute(create_student_table)
+db.execute(create_houses_table)
 
 # Populate houses table if not already done
-# def create_house(db, name, number_of_students)
-  # db.execute("INSERT INTO IF NOT EXISTS houses (name, number_of_students) VALUES ("name")
-# end
+def create_house(db, name, number_of_students)
+  db.execute("INSERT INTO IF NOT EXISTS houses (name, number_of_students) VALUES (?, ?)", [name, number_of_students])
+end
 
-# db.execute(create_student_table)
-# db.execute(create_houses_table)
+create_house('houses', 'Gryffindor', 0)
+create_house('houses', 'Hufflepuff', 0)
+create_house('houses', 'Ravenclaw', 0)
+create_house('houses', 'Slytherin', 0)
 
 # Write methods for the actual quiz...ie how the question answers sort the user
 #
 
 # Write method to create a user
-# def create_student(db, name, gender, house_id, year, quidditch, 2nd_house_id, 3rd_house_id, 4th_house_id)
-#     db.execute("INSERT INTO students (name, gender, house_id, year, quidditch, 2nd_house_id, 3rd_house_id, 4th_house_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", [name, gender, house_id, year, quidditch, 2nd_house_id, 3rd_house_id, 4th_house_id])
-# end
+def create_student(db, name, gender, house_id, year, quidditch, 2nd_house_id, 3rd_house_id, 4th_house_id)
+    db.execute("INSERT INTO students (name, gender, house_id, year, quidditch, 2nd_house_id, 3rd_house_id, 4th_house_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", [name, gender, house_id, year, quidditch, 2nd_house_id, 3rd_house_id, 4th_house_id])
+end
 
 # Write method that edits user's house after taking quiz
   # Problem: how to identify primary key for the user?
