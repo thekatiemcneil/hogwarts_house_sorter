d861540c7b0f019ffb3b9e65c86a7719e1f71429
thekatiemcneil <katie.mcneil@live.com>
2017-06-10 12:23:40 -0500

    Change user input for basic info and for questions to take place within methods.

--- quiz.rb
+++ quiz.rb
@@ -48,19 +48,21 @@ db.execute(create_student_table)
 # create_house(db, 'Slytherin', 0)
 # Commented out 'create house table' items, because it can be created from outside of this ruby program. The data in this table is limited to four houses, so no need to use ruby to make this.
 
-puts "Welcome to Hogwarts School of Witchcraft and Wizardry! We hope you enjoyed your journey to the castle. \nBefore we can enjoy the start-of-term feast, we need to sort you into your house. \nWhat's your full name?"
-  name = gets.chomp
-puts "Thanks! What is your gender?"
-  gender = gets.chomp
-puts "Are you interested in joining your house's quidditch team? y/n"
-  quidditch = gets.chomp
-  if quidditch.downcase == "y"
-    quidditch = "true"
-  elsif quidditch.downcase == "n"
-    quidditch = "false"
-  else
-    quidditch = "false"
-  end
+def begin_sorting
+  puts "Welcome to Hogwarts School of Witchcraft and Wizardry! We hope you enjoyed your journey to the castle. \nBefore we can enjoy the start-of-term feast, we need to sort you into your house. \nWhat's your full name?"
+    name = gets.chomp
+  puts "Thanks! What is your gender?"
+    gender = gets.chomp
+  puts "Are you interested in joining your house's quidditch team? y/n"
+    quidditch = gets.chomp
+    if quidditch.downcase == "y"
+      quidditch = "true"
+    elsif quidditch.downcase == "n"
+      quidditch = "false"
+    else
+      quidditch = "false"
+    end
+end
 
 # Write methods for the actual quiz...ie how the question answers sort the user
 # Consider best options for asking questions
@@ -71,84 +73,86 @@ puts "Are you interested in joining your house's quidditch team? y/n"
 # set 0 values for all houses to begin
 # USE A HASHHHHHHHHH
 house_tally = {
-"Slytherin" => 0,
-"Gryffindor" => 0,
-"Hufflepuff" => 0,
-"Ravenclaw" => 0
+  "Slytherin" => 0,
+  "Gryffindor" => 0,
+  "Hufflepuff" => 0,
+  "Ravenclaw" => 0
 }
 
-puts "Please pick a word: sneak, fight, create, think."
-  word = gets.chomp
-    if word.downcase == "sneak"
-      house_tally["Slytherin"] += 1
-    elsif word.downcase == "fight"
-      house_tally["Gryffindor"] += 1
-    elsif word.downcase == "create"
-      house_tally["Hufflepuff"] += 1
-    else word.downcase == "think"
-      house_tally["Ravenclaw"] += 1
-    end
+def ask_questions
+  puts "Please pick a word: sneak, fight, create, think."
+    word = gets.chomp
+      if word.downcase == "sneak"
+        house_tally["Slytherin"] += 1
+      elsif word.downcase == "fight"
+        house_tally["Gryffindor"] += 1
+      elsif word.downcase == "create"
+        house_tally["Hufflepuff"] += 1
+      else word.downcase == "think"
+        house_tally["Ravenclaw"] += 1
+      end
 
-puts "Please pick a color: pink, slate, purple, orange."
-  color = gets.chomp
-    if color.downcase == "pink"
-      house_tally["Hufflepuff"] += 1
-    elsif color.downcase == "slate"
-      house_tally["Slytherin"] += 1
-    elsif color.downcase == "purple"
-      house_tally["Ravenclaw"] += 1
-    else color.downcase == "orange"
-      house_tally["Gryffindor"] += 1
-    end
+  puts "Please pick a color: pink, slate, purple, orange."
+    color = gets.chomp
+      if color.downcase == "pink"
+        house_tally["Hufflepuff"] += 1
+      elsif color.downcase == "slate"
+        house_tally["Slytherin"] += 1
+      elsif color.downcase == "purple"
+        house_tally["Ravenclaw"] += 1
+      else color.downcase == "orange"
+        house_tally["Gryffindor"] += 1
+      end
 
-puts "Where would you like to go on holiday: Oxford, Auckland, Vancouver, or Las Vegas?"
-  city = gets.chomp
-    if city.downcase == "oxford"
-      house_tally["Ravenclaw"] += 1
-    elsif city.downcase == "auckland"
-      house_tally["Gryffindor"] += 1
-    elsif city.downcase == "vancouver"
-      house_tally["Hufflepuff"] += 1
-    else city.downcase == "las vegas"
-      house_tally["Slytherin"] += 1
-    end
+  puts "Where would you like to go on holiday: Oxford, Auckland, Vancouver, or Las Vegas?"
+    city = gets.chomp
+      if city.downcase == "oxford"
+        house_tally["Ravenclaw"] += 1
+      elsif city.downcase == "auckland"
+        house_tally["Gryffindor"] += 1
+      elsif city.downcase == "vancouver"
+        house_tally["Hufflepuff"] += 1
+      else city.downcase == "las vegas"
+        house_tally["Slytherin"] += 1
+      end
 
-puts "What is your favorite season: Summer, Spring, Autumn, or Winter?"
-  season = gets.chomp
-    if season.downcase == "summer"
-      house_tally["Hufflepuff"] += 1
-    elsif season.downcase == "spring"
-      house_tally["Ravenclaw"] += 1
-    elsif season.downcase == "autumn"
-      house_tally["Gryffindor"] += 1
-    else season.downcase == "winter"
-      house_tally["Slytherin"] += 1
-    end
+  puts "What is your favorite season: Summer, Spring, Autumn, or Winter?"
+    season = gets.chomp
+      if season.downcase == "summer"
+        house_tally["Hufflepuff"] += 1
+      elsif season.downcase == "spring"
+        house_tally["Ravenclaw"] += 1
+      elsif season.downcase == "autumn"
+        house_tally["Gryffindor"] += 1
+      else season.downcase == "winter"
+        house_tally["Slytherin"] += 1
+      end
 
-puts "Pick a variety of beer: stout, IPA, sour, or saison."
-  beer = gets.chomp
-    if beer.downcase == "stout"
-      house_tally["Gryffindor"] += 1
-    elsif beer.downcase == "ipa"
-      house_tally["Hufflepuff"] += 1
-    elsif beer.downcase == "sour"
-      house_tally["Slytherin"] += 1
-    else beer.downcase == "saison"
-      house_tally["Ravenclaw"] += 1
-    end
+  puts "Pick a variety of beer: stout, IPA, sour, or saison."
+    beer = gets.chomp
+      if beer.downcase == "stout"
+        house_tally["Gryffindor"] += 1
+      elsif beer.downcase == "ipa"
+        house_tally["Hufflepuff"] += 1
+      elsif beer.downcase == "sour"
+        house_tally["Slytherin"] += 1
+      else beer.downcase == "saison"
+        house_tally["Ravenclaw"] += 1
+      end
 
-puts "What house do you want to be in?"
-  house_preference = gets.chomp
-    if house_preference.downcase == "gryffindor"
-      house_tally["Gryffindor"] += 2
-    elsif house_preference.downcase == "ravenclaw"
-      house_tally["Ravenclaw"] += 2
-    elsif house_preference.downcase == "hufflepuff"
-      house_tally["Hufflepuff"] += 2
-    elsif house_preference.downcase == "slytherin"
-      house_tally["Slytherin"] += 2
-    else house_tally = house_tally
-    end
+  puts "What house do you want to be in?"
+    house_preference = gets.chomp
+      if house_preference.downcase == "gryffindor"
+        house_tally["Gryffindor"] += 2
+      elsif house_preference.downcase == "ravenclaw"
+        house_tally["Ravenclaw"] += 2
+      elsif house_preference.downcase == "hufflepuff"
+        house_tally["Hufflepuff"] += 2
+      elsif house_preference.downcase == "slytherin"
+        house_tally["Slytherin"] += 2
+      else house_tally = house_tally
+      end
+end
 
 
 # define logic to determine which house has the most points
@@ -196,7 +200,14 @@ def retrieve_tally(db, house_id)
   db.execute("SELECT number_of_students FROM houses WHERE id=#{house_id}")
 end
 
-g
+tally = retrieve_tally(db, house_id)[0]
 
-# def update_tally(db, house_id, new_tally)
+# new_tally = tally[0] += 1
+# p new_tally
 
+def update_tally(db, house_id, tally)
+  new_tally = tally[0] += 1
+  db.execute("UPDATE houses SET number_of_students=#{new_tally} WHERE id=#{house_id}")
+end
+
+# update_tally(db, house_id, tally)
